5fe93ac87fb807876adec45db3fa49c396cf7d97
iguin26 <igordaniel.lopes@gmail.com>
2025-04-30 15:47:22 -0300

    define routes

--- server.js
+++ server.js
@@ -1,3 +1,29 @@
 import express from "express";
+import "dotenv/config";
 
-console.log("start");
+const PORT = process.env.PORT;
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`Server running in port ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.status(200).json({ msg: "get home" });
+});
+
+app.get("auth/login", (req, res) => {
+  res.status(200).send({ msg: "get login page" });
+});
+
+app.get("auth/register", (req, res) => {
+  res.status(200).send({ msg: "get register page" });
+});
+
+app.get("/courses/:page", (req, res) => {
+  res.status(200).send({ msg: "get cours es  page" });
+});
+
+app.get("/faq", (req, res) => {
+  res.status(200).send({ msg: "get faq page" });
+});
