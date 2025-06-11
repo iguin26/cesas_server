405a346a0dea8f39c213199b6d187ae37708bad0
MatheusCastro <matheusnc2004@gmail.com>
2025-06-11 00:31:18 -0300

    auth

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,31 +1,20 @@
-import { where } from 'sequelize';
-import { findOne } from '../models/Admin.js';
-import { compare } from 'bcrypt';
+import admin from '../models/Admin.js';
+import bcrypt from 'bcrypt';
 
-async function authenticateAdmin(enteredPassword){
-  try{
-    //pega o adminn pelo nome de usuario
-    const admin = await findOne({where: {username: 'admin'}});
-
-    if(!admin){
-      console.log('Admin nao encontrado');
-      return false;
-    }
-
-    //consfere se a senha bate comm o hash do banco
-    const match = await compare(enteredPassword, admin.password);
-
-    if(match){
-      console.log('Autenticacao bem feita');
-      return true;
-    }else{
-      console.log("Senha incorreta");
-      return false;
-    }
-
-  }catch(erro){
-    console.error('Erro na autenticacao', erro);
-    return false;
-  }
+const DEFAULT_ADMIN = {
+  email: admin.findOne({
+    attributes: ['login'],
+  }),
+  password: admin.findOne({
+    attributes: ['password']
+  })
 }
 
+  export const authenticate = async(email, password) => {
+    if(email === DEFAULT_ADMIN.email && compare(password, admin.password)){
+      return Promise.resolve(DEFAULT_ADMIN);
+
+    }
+    return null;
+
+};
